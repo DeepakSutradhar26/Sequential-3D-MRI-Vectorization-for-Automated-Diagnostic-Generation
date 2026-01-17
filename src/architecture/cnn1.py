56af69bc7f1353e2ee3b22ccdfdde48a512a9cbd
Deepa <deepaksutradhar415@gmail.com>
2026-01-17 13:50:03 +0530

    changes

--- src/architecture/cnn1.py
+++ src/architecture/cnn1.py
@@ -30,6 +30,11 @@ class CNNArchitecture(nn.Module):
             nn.Conv3d(in_channels, out_channels, kernel_size=3, padding=1),
             nn.BatchNorm3d(out_channels),
             nn.ReLU(inplace=True),
+
+            nn.Conv3d(out_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm3d(out_channels),
+            nn.ReLU(inplace=True),
+
             nn.MaxPool3d(kernel_size=2, stride=2),
         )
     
