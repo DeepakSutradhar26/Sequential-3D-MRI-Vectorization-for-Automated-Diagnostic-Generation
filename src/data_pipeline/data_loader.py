ebd045a2dbb5b3164097ee2b791ba3f551d17ed1
Deepa <deepaksutradhar415@gmail.com>
2026-01-20 10:39:43 +0530

    bugs-fix

--- src/data_pipeline/data_loader.py
+++ src/data_pipeline/data_loader.py
@@ -36,3 +36,8 @@ val_loader = DataLoader(
     shuffle = False,
     pin_memory = True,
 )
+
+for x, y in train_loader:
+    print("x shape:", x.shape)
+    print("y shape:", y.shape)
+    break  # only print first batch
--- src/data_pipeline/dataset.py
+++ src/data_pipeline/dataset.py
@@ -30,6 +30,8 @@ class MRIDataset(Dataset):
         x = torch.stack(volumes) #(4, D, H, W)
         x = x.unsqueeze(1) # (4, 1, D, H, W)
 
+        print("dataset.py", x.shape)
+
         label = 1 if label_name == "HGG" else 0
         y = torch.tensor(label).unsqueeze(0)
 
